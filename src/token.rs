b7469eff04341e638588ddf22a9c6e5efbfdf4d6
SirTimme <tim@sirtimme.dev>
2024-12-05 10:53:25 +0100

    reorganize lexer tests

--- src/token.rs
+++ src/token.rs
@@ -79,23 +79,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn when_valid_tokens_expect_lexing_succeeds() {
+    fn when_valid_characters_expect_lexing_succeeds() {
         let input = "T6|w(4294967298)|59";
+        
         let result = tokenize_source(input.to_string());
-
         assert!(result.is_ok());
-        assert_eq!(result.unwrap().len(), 8);
+        
+        let tokens = result.unwrap();
+        assert_eq!(tokens.len(), 8);
     }
 
     #[test]
-    fn when_invalid_tokens_expect_lexing_fails() {
-        let input = "T6|w(4294967298)*|59";
+    fn when_invalid_characters_expect_lexing_fails() {
+        let input = "T6|w(4294967298)*|59"; // '*' is an invalid character
+        
         let result = tokenize_source(input.to_string());
-
         assert!(result.is_err());
-        assert_eq!(
-            result.unwrap_err().to_string(),
-            "Logos encountered an non-ascii character"
-        );
+        
+        let error = result.unwrap_err();
+        assert_eq!(error.to_string(), "Logos encountered an non-ascii character");
     }
 }
